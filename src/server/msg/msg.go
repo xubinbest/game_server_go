b677c950f70ae233df2d55df8178c7116b3f8ee8
unknown <xubinbest@gmail.com>
2024-07-29 18:27:55 +0800

    提交代码

--- src/server/msg/msg.go
+++ src/server/msg/msg.go
@@ -1,9 +1,12 @@
 package msg
 
-import "github.com/name5566/leaf/network"
+import (
+    "github.com/name5566/leaf/network"
+    "github.com/name5566/leaf/network/protobuf"
+)
 
 var Processor network.Processor
 
 func init() {
-
+    Processor = new(protobuf.Processor)
 }
